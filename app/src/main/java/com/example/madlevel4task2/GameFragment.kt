d39ab6537ba82bbb55ef2553bf59dfe014986a65
M-BenAli-HvA <mohamed.ben.ali@hva.nl>
2020-09-21 01:12:27 +0200

    Update Game model and GameFragment logic

--- app/src/main/java/com/example/madlevel4task2/GameFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/GameFragment.kt
@@ -1,12 +1,22 @@
 package com.example.madlevel4task2
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.madlevel4task2.models.Game
+import com.example.madlevel4task2.models.GameMoves
+import com.example.madlevel4task2.models.GameResult
+import kotlinx.android.synthetic.main.fragment_game.*
+import java.time.Instant
+import java.util.*
 
-class GameFragment: Fragment() {
+class GameFragment : Fragment() {
+
+    private val TAG = "GameFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +28,46 @@ class GameFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+        img_btn_paper.setOnClickListener {
+            Log.i(TAG, "Clicked on paper image button")
+            createGame(GameMoves.PAPER)
+        }
+
+        img_btn_rock.setOnClickListener {
+            Log.i(TAG, "Clicked on rock image button")
+            createGame(GameMoves.ROCK)
+        }
+
+        img_btn_scissors.setOnClickListener {
+            Log.i(TAG, "Clicked on scissors image button")
+            createGame(GameMoves.SCISSORS)
+        }
+
+    }
+
+    private fun createGame(playerMove: GameMoves) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val computerMove = GameMoves.values()[(0..2).random()]
+            val game = Game(
+                playerMove, computerMove,
+                Date.from(Instant.now())
+            )
+            game.startGame()
+            Log.i(TAG, game.toString())
+            displayResult(game)
+        }
+    }
+
+
+    private fun displayResult(game: Game) {
+        when(game.result) {
+            GameResult.WIN -> tv_result_message.setText(R.string.win_label)
+            GameResult.DRAW-> tv_result_message.setText(R.string.draw_label)
+            GameResult.LOSE -> tv_result_message.setText(R.string.lose_label)
+        }
     }
 }
--- app/src/main/java/com/example/madlevel4task2/models/Game.kt
+++ app/src/main/java/com/example/madlevel4task2/models/Game.kt
@@ -1,4 +1,61 @@
 package com.example.madlevel4task2.models
 
-class Game {
+import java.lang.StringBuilder
+import java.util.*
+
+
+enum class GameMoves {
+    ROCK,
+    PAPER,
+    SCISSORS
+}
+
+enum class GameResult {
+    WIN,
+    DRAW,
+    LOSE
+}
+
+class Game(
+    var playerMove: GameMoves, var computerMove: GameMoves,
+    var date: Date, var result: GameResult? = null
+) {
+
+    fun startGame() {
+        determineResult()
+    }
+
+    private fun determineResult() {
+        if (playerMove == computerMove) {
+            result = GameResult.DRAW
+        } else {
+            if (playerMove == GameMoves.ROCK) {
+                if (computerMove == GameMoves.PAPER) {
+                    result = GameResult.LOSE
+                } else if (computerMove == GameMoves.SCISSORS) {
+                    result = GameResult.WIN
+                }
+            } else if (playerMove == GameMoves.PAPER) {
+                if (computerMove == GameMoves.SCISSORS) {
+                    result = GameResult.LOSE
+                } else if (computerMove == GameMoves.ROCK) {
+                    result = GameResult.WIN
+                }
+            } else if (playerMove == GameMoves.SCISSORS) {
+                if (computerMove == GameMoves.ROCK) {
+                    result = GameResult.LOSE
+                } else if (computerMove == GameMoves.PAPER) {
+                    result = GameResult.WIN
+                }
+            }
+        }
+    }
+
+
+    override fun toString(): String {
+        return StringBuilder("Player move: ").append(playerMove.name + "\n")
+            .append("Computer move: ").append(computerMove.name + "\n")
+            .append("Date: ").append(date).append("\n")
+            .append("Result: ").append(result?.name).toString()
+    }
 }
