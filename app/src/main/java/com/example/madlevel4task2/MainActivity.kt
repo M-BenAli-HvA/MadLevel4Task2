c0a6054fec5fee6c857eb52c55667eee2fc5d5c1
M-BenAli-HvA <mohamed.ben.ali@hva.nl>
2020-09-22 00:44:41 +0200

    Complete action bar buttons logic

--- app/src/main/java/com/example/madlevel4task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel4task2/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.madlevel4task2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 
@@ -17,13 +17,20 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         navController = findNavController(R.id.nav_host_fragment)
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        invalidateOptionsMenu()
         return true
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        if (menu != null) {
+            menuBtnToggle(menu)
+        }
+        return super.onPrepareOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -34,6 +41,21 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun menuBtnToggle(menu: Menu) {
+        val gameHistoryButton = menu.findItem(R.id.game_history)
+        val clearGameHistory = menu.findItem(R.id.clear_game_history)
+        navController.addOnDestinationChangedListener { _, destination,
+                                                        _ ->
+            if (destination.id in arrayOf(R.id.GameHistory)) {
+                gameHistoryButton.isVisible = false
+                clearGameHistory.isVisible = true
+            } else if (destination.id in arrayOf(R.id.GameFragment)) {
+                gameHistoryButton.isVisible = true
+                clearGameHistory.isVisible = false
+            }
         }
     }
 }
